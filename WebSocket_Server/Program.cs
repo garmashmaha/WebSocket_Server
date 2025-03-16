52d6ee1798af9e371a75a12bc3272b501706c99a
Roman Kapuścik <70838513+RomanKapuscik@users.noreply.github.com>
2025-03-16 11:01:15 +0100

    First commit

---
+++ WebSocket_Server/Program.cs
@@ -0,0 +1,32 @@
+﻿using WebSocketSharp;
+using WebSocketSharp.Server;
+
+namespace WebSocket_Server;
+
+public class TestService : WebSocketBehavior
+{
+    protected override void OnMessage(MessageEventArgs e)
+    {
+        Console.WriteLine("Received from client: " + e.Data);
+        
+        Send("Data from server");
+    }
+
+    protected override void OnError(WebSocketSharp.ErrorEventArgs e)
+    {
+        // do nothing
+    }
+}
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var ws = new WebSocketServer("ws://localhost:8081");
+
+        ws.AddWebSocketService<TestService>("/test");
+        ws.Start();
+        Console.ReadKey(true);
+        ws.Stop();      
+    }
+}
